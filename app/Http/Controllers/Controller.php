502957a53cea62c193108c942f7c9a11002b4c6b
Chris H <cholladay0816@gmail.com>
2020-11-24 15:34:29 -0600

    Init

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Storage;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        return response(file_get_contents(public_path('img/austin.png')),'200',["Content-Type"=>'image/png']);
+    }
 }
