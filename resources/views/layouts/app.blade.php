ef5137bb67467bb52488b0b65be28691d4571a2f
Chris H <cholladay0816@gmail.com>
2020-11-28 15:04:57 -0600

    embeds

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <meta content="{{ config('app.name', 'Laravel') }}" property="og:title">
+        <meta content="The official host of all things failfish." property="og:description">
+        <meta content="Failfish.com" property="og:site_name">
+        <meta content='{{url('/austinoftheday')}}' property='og:image'>
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
