1fcdf349546d3a2e657d33a811e8c44bb9b3dba9
Chris H <cholladay0816@gmail.com>
2020-12-25 15:04:27 -0600

    Update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [Controller::class, 'index']);
-Route::get('/austinoftheday', [\App\Http\Controllers\ImageController::class, 'showDaily']);
+// Route::get('/', [Controller::class, 'index']);
+// Route::get('/austinoftheday', [\App\Http\Controllers\ImageController::class, 'showDaily']);
+Route::get('/', [Controller::class, 'showDaily']);
 
 Route::group(['auth:sanctum', 'verified'], function() {
 
