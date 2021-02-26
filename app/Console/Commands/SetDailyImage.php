18ef9109d5b86226d24c0ee3571d674485f6fb1c
Christian Holladay <cholladay0816@gmail.com>
2021-02-26 12:29:54 -0600

    Update SetDailyImage.php

--- app/Console/Commands/SetDailyImage.php
+++ app/Console/Commands/SetDailyImage.php
@@ -48,22 +48,22 @@ class SetDailyImage extends Command
         }
         if ($christmas->isCurrentDay()) {
             $this->comment('Today is Christmas');
-            $image = Image::where('name', '=', 'austingrinch')->first();
+            $image = Image::where('name', 'austingrinch')->first();
             $image->active = 1;
             $image->save();
         } elseif ($thanksgiving->isCurrentDay()) {
             $this->comment('Today is Thanksgiving');
-            $image = Image::where('name', '=', 'austinturkey')->first();
+            $image = Image::where('name', 'austinturkey')->first();
             $image->active = 1;
             $image->save();
         } elseif ($easter->isCurrentDay()) {
             $this->comment('Today is Easter');
-            $image = Image::where('name', '=', 'austineaster')->first();
+            $image = Image::where('name', 'austineaster')->first();
             $image->active = 1;
             $image->save();
         } elseif (now()->format('d') == '01') {
             $this->comment('Today is the 1st of the month');
-            $image = Image::where('name', '=', 'austinfirst')->first();
+            $image = Image::where('name', 'austinfirst')->first();
             $image->active = 1;
             $image->save();
         } else {
@@ -71,12 +71,12 @@ class SetDailyImage extends Command
             $this->comment('Today is a normal '.$weekday);
 
 
-            $daily = Image::where('name', '=', 'austin'.strtolower(now()->dayName))->first();
+            $daily = Image::where('name', 'austin'.strtolower(now()->dayName))->first();
             if (isset($daily)) {
                 $daily->active = 1;
                 $daily->save();
             } else {
-                $image = Image::where('name', '=', 'austin')->first();
+                $image = Image::where('name', 'austin')->first();
                 $image->active = 1;
                 $image->save();
             }
