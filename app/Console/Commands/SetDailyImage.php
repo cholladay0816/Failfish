5882d6c42131892b61d3c61b587bdd5187703d7c
Christian Holladay <cholladay0816@gmail.com>
2021-08-03 18:37:22 -0500

    THE JERMA UPDATE

--- app/Console/Commands/SetDailyImage.php
+++ app/Console/Commands/SetDailyImage.php
@@ -38,49 +38,49 @@ class SetDailyImage extends Command
      */
     public function handle()
     {
-        $christmas = now()->month(12)->day(25);
-        $thanksgiving = now()->month(11)->lastOfMonth(4);
-        $easter = Image::calculateEaster();
-        $images = Image::all();
-        foreach ($images as $image) {
-            $image->active = 0;
-            $image->save();
-        }
-        if ($christmas->isCurrentDay()) {
-            $this->comment('Today is Christmas');
-            $image = Image::where('name', 'austingrinch')->first();
-            $image->active = 1;
-            $image->save();
-        } elseif ($thanksgiving->isCurrentDay()) {
-            $this->comment('Today is Thanksgiving');
-            $image = Image::where('name', 'austinturkey')->first();
-            $image->active = 1;
-            $image->save();
-        } elseif ($easter->isCurrentDay()) {
-            $this->comment('Today is Easter');
-            $image = Image::where('name', 'austineaster')->first();
-            $image->active = 1;
-            $image->save();
-        } elseif (now()->format('d') == '01') {
-            $this->comment('Today is the 1st of the month');
-            $image = Image::where('name', 'austinfirst')->first();
-            $image->active = 1;
-            $image->save();
-        } else {
-            $weekday = strtolower(now()->dayName);
-            $this->comment('Today is a normal '.$weekday);
-
-
-            $daily = Image::where('name', 'austin'.strtolower(now()->dayName))->first();
-            if (isset($daily)) {
-                $daily->active = 1;
-                $daily->save();
-            } else {
-                $image = Image::where('name', 'austin')->first();
-                $image->active = 1;
-                $image->save();
-            }
-        }
+//        $christmas = now()->month(12)->day(25);
+//        $thanksgiving = now()->month(11)->lastOfMonth(4);
+//        $easter = Image::calculateEaster();
+//        $images = Image::all();
+//        foreach ($images as $image) {
+//            $image->active = 0;
+//            $image->save();
+//        }
+//        if ($christmas->isCurrentDay()) {
+//            $this->comment('Today is Christmas');
+//            $image = Image::where('name', 'austingrinch')->first();
+//            $image->active = 1;
+//            $image->save();
+//        } elseif ($thanksgiving->isCurrentDay()) {
+//            $this->comment('Today is Thanksgiving');
+//            $image = Image::where('name', 'austinturkey')->first();
+//            $image->active = 1;
+//            $image->save();
+//        } elseif ($easter->isCurrentDay()) {
+//            $this->comment('Today is Easter');
+//            $image = Image::where('name', 'austineaster')->first();
+//            $image->active = 1;
+//            $image->save();
+//        } elseif (now()->format('d') == '01') {
+//            $this->comment('Today is the 1st of the month');
+//            $image = Image::where('name', 'austinfirst')->first();
+//            $image->active = 1;
+//            $image->save();
+//        } else {
+//            $weekday = strtolower(now()->dayName);
+//            $this->comment('Today is a normal '.$weekday);
+//
+//
+//            $daily = Image::where('name', 'austin'.strtolower(now()->dayName))->first();
+//            if (isset($daily)) {
+//                $daily->active = 1;
+//                $daily->save();
+//            } else {
+//                $image = Image::where('name', 'austin')->first();
+//                $image->active = 1;
+//                $image->save();
+//            }
+//        }
         return 0;
     }
 }
